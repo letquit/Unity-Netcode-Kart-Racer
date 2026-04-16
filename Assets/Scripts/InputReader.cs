0d591ad472d3465768a37fc02da4b97e39588875
letquit <1446444589@qq.com>
2026-04-16 23:50:07 +0800

    Initial commit

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Kart
+{
+    [CreateAssetMenu(fileName = "InputReader", menuName = "Kart/Input Reader")]
+    public class InputReader : ScriptableObject, PlayerInputActions.IPlayerActions
+    {
+        public Vector2 Move => inputActions.Player.Move.ReadValue<Vector2>();
+        public bool IsBraking => inputActions.Player.Brake.ReadValue<float>() > 0;
+        
+        private PlayerInputActions inputActions;
+
+        private void OnEnable()
+        {
+            if (inputActions == null)
+            {
+                inputActions = new PlayerInputActions();
+                inputActions.Player.SetCallbacks(this);
+            }
+            inputActions.Enable();
+        }
+
+        public void Enable()
+        {
+            inputActions.Enable();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            // noop
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            // noop
+        }
+
+        public void OnFire(InputAction.CallbackContext context)
+        {
+            // noop
+        }
+
+        public void OnBrake(InputAction.CallbackContext context)
+        {
+            // noop
+        }
+    }
+}
---
+++ Assets/Scripts/KartController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using UnityEngine;
+using Utilities;
+
+namespace Kart
+{
+    [Serializable]
+    public class AxleInfo
+    {
+        public WheelCollider leftWheel;
+        public WheelCollider rightWheel;
+        public bool motor;
+        public bool steering;
+        public WheelFrictionCurve originalForwardFriction;
+        public WheelFrictionCurve originalSidewaysFriction;
+    }
+
+    public class KartController : MonoBehaviour
+    {
+        [Header("Axle Information")]
+        [SerializeField] private AxleInfo[] axleInfos;
+        
+        [Header("Motor Attributes")]
+        [SerializeField] private float maxMotorTorque = 3000f;
+        [SerializeField] private float maxSpeed;
+        
+        [Header("Steering Attributes")]
+        [SerializeField] private float maxSteeringAngle = 30f;
+
+        [Header("Braking and Drifting Attributes")] 
+        [SerializeField] private float brakeTorque = 10000f;
+
+        [SerializeField] private InputReader input;
+        private Rigidbody rb;
+
+        private float brakeVelocity;
+        
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            input.Enable();
+
+            foreach (AxleInfo axleInfo in axleInfos)
+            {
+                axleInfo.originalForwardFriction = axleInfo.leftWheel.forwardFriction;
+                axleInfo.originalSidewaysFriction = axleInfo.leftWheel.sidewaysFriction;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            float verticalInput = AdjustInput(input.Move.y);
+            float horizontalInput = AdjustInput(input.Move.x);
+            
+            float motor = maxMotorTorque * verticalInput;
+            float steering = maxSteeringAngle * horizontalInput;
+
+            UpdateAxles(motor, steering);
+        }
+
+        private void UpdateAxles(float motor, float steering)
+        {
+            foreach (AxleInfo axleInfo in axleInfos)
+            {
+                HandleSteering(axleInfo, steering);
+                HandleMotor(axleInfo, motor);
+                HandleBrakesAndDrift(axleInfo);
+                HandleWheelVisuals(axleInfo.leftWheel);
+                HandleWheelVisuals(axleInfo.rightWheel);
+            }
+        }
+
+        private void HandleWheelVisuals(WheelCollider collider)
+        {
+            if (collider.transform.childCount == 0) return;
+            
+            Transform visualWheel = collider.transform.GetChild(0);
+
+            Vector3 position;
+            Quaternion rotation;
+            collider.GetWorldPose(out position, out rotation);
+
+            visualWheel.transform.position = position;
+            visualWheel.transform.rotation = rotation;
+        }
+
+        private void HandleSteering(AxleInfo axleInfo, float steering)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+        }
+
+        private void HandleMotor(AxleInfo axleInfo, float motor)
+        {
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+
+        private void HandleBrakesAndDrift(AxleInfo axleInfo)
+        {
+            if (axleInfo.motor)
+            {
+                if (input.IsBraking)
+                {
+                    rb.constraints = RigidbodyConstraints.FreezeRotationX;
+
+                    float newZ = Mathf.SmoothDamp(rb.linearVelocity.z, 0, ref brakeVelocity, 1f);
+                    rb.linearVelocity = rb.linearVelocity.With(z: newZ);
+                    
+                    axleInfo.leftWheel.brakeTorque = brakeTorque;
+                    axleInfo.rightWheel.brakeTorque = brakeTorque;
+                }
+                else
+                {
+                    rb.constraints = RigidbodyConstraints.None;
+
+                    axleInfo.leftWheel.brakeTorque = 0;
+                    axleInfo.rightWheel.brakeTorque = 0;
+                }
+            }
+        }
+
+        private float AdjustInput(float input)
+        {
+            return input switch
+            {
+                >= .7f => 1f,
+                <= -.7f => -1f,
+                _ => input
+            };
+        }
+    }
+}
